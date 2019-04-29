edba220e7027397c3c36059246ccea400d15be92
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-04-29 17:23:16 +0200

    navbar update

--- index.php
+++ index.php
@@ -1,38 +1,135 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-   <head>
-      <meta charset="utf-8">
-      <title>Amazon ECE</title>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">
-   </head>
-   <body>
 
-      <!--NAVBAR-->
-      <?php include("views/navbar.html") ?>
+<head>
+   <meta charset="utf-8">
+   <title>Amazon ECE</title>
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <link rel="stylesheet" href="style.css">
+</head>
 
-      <!--BEST SELL-->
-      <div class="container">
-         <div class="row">
-            <div class="col-3">
-               Item 1
-            </div>
-            <div class="col-3">
-Item 2
-            </div>
-            <div class="col-3">
-Item 3
-            </div>
-            <div class="col-3">
-Item 4
-            </div>
+<body>
+
+   <!--NAVBAR-->
+   <?php include("views/navbar.html") ?>
+
+   <!--BEST SELL-->
+
+   <div class="container my-5 shadow" id="best-sell">
+      <h2>Ventes Flash</h2>
+      <div class="row">
+         <div class="col-3 col-best-sell">
+            <img src="img/who.jpg" alt="who">
+            <div id="comment-in-img">The Who</div>
+         </div>
+         <div class="col-3 col-best-sell">
+            <img src="img/rhcp.jpg" alt="rhcp">
+            <div id="comment-in-img">Red Hot Chili Peppers</div>
+         </div>
+         <div class="col-3 col-best-sell">
+            <img src="img/tp.jpg" alt="tp">
+            <div id="comment-in-img">The Police</div>
+         </div>
+         <div class="col-3 col-best-sell">
+            <img src="img/am.jpg" alt="am">
+            <div id="comment-in-img">Arctic Monkeys</div>
          </div>
       </div>
+   </div>
 
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   </body>
+   <div class="container my-5 " id="categorie">
+      <div class="row">
+         <div class="col-3 my-3 shadow-sm" style="text-align:center">
+            <a href="#">
+               <h3>Livres</h3>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+               </div>
+            </a>
+         </div>
+         <div class="col-3 my-3 shadow-sm" style="text-align:center">
+            <a href="#">
+               <h3>Musique</h3>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+               </div>
+            </a>
+         </div>
+         <div class="col-3 my-3 shadow-sm" style="text-align:center">
+            <a href="#">
+               <h3>Vêtements</h3>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+               </div>
+            </a>
+         </div>
+         <div class="col-3 my-3 shadow-sm" style="text-align:center">
+            <a href="#">
+               <h3>Sports & Loisir</h3>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-6">
+                     <img src="img/tp.jpg" alt="">
+                  </div>
+                  <div class="col-6">
+                     <img src="img/am.jpg" alt="">
+                  </div>
+               </div>
+            </a>
+         </div>
+      </div>
+   </div>
+
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
