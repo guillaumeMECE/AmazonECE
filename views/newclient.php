14ffb7b7839e12342748170a9dfb16ecb8f59332
clairedu92_github <claire.grouhel@edu.ece.fr>
2019-04-30 20:44:11 +0200

    new version entrée client bdd

--- views/newclient.php
+++ views/newclient.php
@@ -16,37 +16,34 @@
 
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputnom">Nom</label>
-      <input type="Nom" class="form-control" id="input_name" placeholder="Nom">
+      <label for="name">Nom</label>
+      <input type="Nom" class="form-control" name ="name" id="iname" placeholder="Nom">
     </div>
 
       <div class="form-group col-md-6">
-        <label for="inputprenom">Prenom</label>
-        <input type="Prenom" class="form-control" name = "name" id ="name" placeholder="Prenom">
+        <label for="firstname">Prenom</label>
+        <input type="text" class="form-control" name = "firstname" id ="name" placeholder="Prenom">
       </div>
 
 
   <div class="form-group">
-    <label for="inputAddress">Addresse Ligne 1</label>
+    <label for="input_Address1">Addresse Ligne 1</label>
     <input type="text" class="form-control" name ="input_Address1" placeholder="Ex : 6 rue de Paris">
   </div>
-  <div class="form-group">
-    <label for="inputAddress2">Addresse Ligne 2</label>
-    <input type="text" class="form-control" name ="input_Address2" placeholder="Appartment, studio, etage ...">
-  </div>
+
     <div class="form-group">
-      <label for="inputCity">Ville</label>
+      <label for="input_City">Ville</label>
       <input type="text" class="form-control" name ="input_City" placeholder = "ex: Nice">
     </div>
     <div class="form-group">
-      <label for="inputCodepostal">Code Postal</label>
+      <label for="input_codepostal">Code Postal</label>
       <input type="text" class="form-control" name ="input_codepostal" placeholder = "ex: 75015">
     </div>
 
 <div class="form-group">
   <div class="form-group col-md-8">
-      <label for="inputState">Pays</label>
-      <select id="input_State" class="form-control">
+      <label for="input_State">Pays</label>
+      <select name ="input_State" class="form-control">
         <option selected>Choisir...</option>
         <option>France</option>
         <option>Belgique</option>
@@ -62,17 +59,17 @@
 
       <div class="form-group col-md-6">
         <label for="input_tel">Telephone</label>
-        <input type="Telephone" class="form-control" name="input_tel" placeholder="ex : 0662452331">
+        <input type="text" class="form-control" name="input_tel" placeholder="ex : 0662452331">
       </div>
 
 
       <div class="form-group col-md-6">
-        <label for="input_mail">Mail</label>
+        <label for="mail">Mail</label>
         <input type="Mail" class="form-control" name="mail" placeholder="ex : Lea@edu.ece.fr">
       </div>
 
       <div class="form-group col-md-6">
-        <label for="input_password">Mot de passe</label>
+        <label for="password">Mot de passe</label>
         <input type="password" class="form-control" name = "password" id="password" >
       </div>
 </div>
@@ -82,7 +79,8 @@
     <div class="p-3 mb-2 bg-secondary text-white">Donnees Bancaires</div>
 
   <div class = "form-group col-md-6">
-      Type de Carte <select type="text" name= type_carte class = "form-control">
+    <label for ="type_carte">  Type de Carte </label>
+    <select type="text" name= type_carte class = "form-control">
 <option value="" ></option>
 <option value="MasterCard" >MasterCard</option>
 <option value="Visa">Visa</option>
@@ -92,22 +90,22 @@
   </div>
 
   <div class = "form-group col-md-6">
-  <label for ="inputnumerocartebleue">Numero carte bancaire</label>
-   <input  class = "form-control" name="numero_carte" placeholder="ex : 1234..."/>
+  <label for ="numero_carte">Numero carte bancaire</label>
+   <input  type="text" class = "form-control" name="numero_carte" placeholder="ex : 1234..."/>
    </div>
 
   <div class="form-group col-md-6">
-      <label for="inputcartebleue">Nom sur la carte bleue</label>
+      <label for="input_nomcb">Nom sur la carte bleue</label>
       <input  class="form-control" name="input_nomcb" placeholder="Nom sur la carte bancaire">
     </div>
 
     <div class="form-group col-md-6">
-        <label for="inputdateexpiration">Date d'expiration</label>
+        <label for="input_dateexpiration">Date d'expiration</label>
         <input type="date" class="form-control" name="input_dateexpiration" placeholder="19/03/2021">
       </div>
 
     <div class = "form-group col-md-6">
-  <label for = "iputcodesecurite">Code de Securite</label>
+  <label for = "iput_codesecurite">Code de Securite</label>
   <input class = "form-control" type = "password" name ="input_codesecurite"  maxlength = "3" placeholder="Cryptogramme" />
   <br>
   </div>
--- views/saisie_newclient.php
+++ views/saisie_newclient.php
@@ -3,13 +3,14 @@
     //si le BDD existe, faire le traitement
 
     $name = isset($_POST["name"])? $_POST["name"] : "";
+    $prenom = isset($_POST["firstname"])? $_POST["firstname"] : "";
       $password = isset($_POST["password"])? $_POST["password"] : "";
 
        $mail = isset($_POST["mail"])? $_POST["mail"] : "";
 
-       $adresse1= isset($_POST["input_Adress1"])? $_POST["input_Adress1"]: "";
-       $adresse2= isset($_POST["input_Adress2"])? $_POST["input_Adress2"] :"";
-       $adresse = $adresse1.$adresse2;
+       $adresse= isset($_POST["input_Address1"])? $_POST["input_Address1"]: "";
+
+
        $ville = isset($_POST["input_City"])? $_POST["input_City"] : "";
        $codepostal = isset($_POST["input_codepostal"])? $_POST["input_codepostal"] : "";
        $pays = isset($_POST["input_State"])? $_POST["input_State"] : "";
@@ -20,21 +21,30 @@
        $crypto = isset($_POST["input_codesecurite"])? $_POST["input_codesecurite"] : "";
        $typecarte = isset($_POST["type_carte"])? $_POST["type_carte"] : "";
 
-        $database = "piscine.sql";    //connectez-vous dans votre BDD  //Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
-         $db_handle = mysqli_connect('localhost', 'root', '');
-          $db_found = mysqli_select_db($db_handle, $database);
+require_once("../config/db.php");
+       $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+       // Check connection
+       if (!$conn) {
+           die("Connection failed: " . mysqli_connect_error());
+       }
 
 
-  if ($db_found) {
+       else if($conn){
     if ($name!="" && $mail!="")
     {
-    $sql = "INSERT INTO buyer VALUES (name, password, mail, adresse, ville, cp, pays, tel)
-    VALUES('".$name."', '".$password."', '".$mail."', '".$adresse."', '".$ville."', '".$codepostal."','".$pays."','" .$tel."')";
-    echo ("ok");
-     $result = mysqli_query($db_handle, $sql);
+    $sql = "INSERT INTO buyer (name,firstname, password, mail, adresse, ville, cp, pays, tel)
+    VALUES('".$name."','".$prenom."', '".$password."', '".$mail."', '".$adresse."', '".$ville."', '".$codepostal."','".$pays."','" .$tel."');";
+    $result = mysqli_query($conn, $sql);
 
-echo ($name);
-echo ($ville);
+    $sql1 = "INSERT INTO card (nomcarte, numero, datefin, crypto, type)
+    VALUES('".$nomcard."','".$card."', '".$datefin."', '".$crypto."', '".$typecarte."');";
+    $result = mysqli_query($conn, $sql1);
+    header('Location : ../index.php');
+    ?>
+    <div class="alert alert-success" role="alert">
+  Felicitation vous avez bien été inscrit!
+</div>
+<?php
 }
 else
 {
@@ -46,21 +56,9 @@ else
   <?php
   require "newclient.php";
 
-
 }
-
 }
-      else {
-      echo ("Database not found");
-     }
-
-
-
-
-
-
-
-      mysqli_close($db_handle);
+      mysqli_close($conn);
 
 
 ?>
